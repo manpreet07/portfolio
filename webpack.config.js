daf198d7cd2459361a48c590dfd025bcf078692b
manpreet07 <manpreet1107@gmail.com>
2023-12-03 14:51:57 -0500

    wp

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
-export const plugins = [
-  new ModuleFederationPlugin({
-    name: "ReactMicrofrontend",
-    filename: "remoteEntry.js",
-    exposes: {
-      "./App": "./build",
-    },
-    shared: {},
-  }),
-];
+module.exports = {
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "ReactMicrofrontend",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./build",
+      },
+      shared: {},
+    }),
+  ],
+};
