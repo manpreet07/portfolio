0a127cdd7d583d84b0578cb407c20d87a5a17069
Manpreet Singh <manpreet1107@gmail.com>
2025-04-04 11:36:00 -0400

    vite (#1)

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Container,
   Table,
+  TableBody,
   TableCell,
   TableContainer,
   TableHead,
@@ -16,129 +17,137 @@ export function Skills() {
       <TableContainer>
         <Table>
           <TableHead>
-            <Typography variant="h5">Skills</Typography>
+            <TableRow>
+              <TableCell>
+                <Typography variant="h5">Skills</Typography>
+              </TableCell>
+            </TableRow>
           </TableHead>
-          <TableRow>
-            <TableCell>
-              <Typography variant="subtitle1">Programming Languages</Typography>
-            </TableCell>
-            <TableCell>
-              <Box sx={{ display: "flex", gap: "10px" }}>
-                <Button
-                  variant="contained"
-                  size="small"
-                  href="https://www.typescriptlang.org/"
-                  style={{ backgroundColor: "#007acc", color: "white" }}
-                >
-                  Typescript
-                </Button>
-                <Button
-                  variant="contained"
-                  size="small"
-                  href="https://www.typescriptlang.org/"
-                  style={{ backgroundColor: "#fcdc00", color: "black" }}
-                >
-                  Javascript
-                </Button>
-                <Button
-                  variant="contained"
-                  size="small"
-                  href="https://www.python.org/"
-                  style={{
-                    backgroundColor: "#4584b6",
-                    color: "white",
-                  }}
-                >
-                  Python
-                </Button>
-                <Button
-                  variant="contained"
-                  size="small"
-                  href="https://www.java.com/en/"
-                  style={{
-                    backgroundColor: "#f89820",
-                    color: "white",
-                  }}
-                >
-                  Java
-                </Button>
-                <Button
-                  variant="contained"
-                  size="small"
-                  href="https://learn.microsoft.com/en-us/dotnet/csharp/"
-                  style={{ backgroundColor: "green", color: "white" }}
-                >
-                  C#
-                </Button>
-                <Button
-                  variant="contained"
-                  size="small"
-                  href="https://www.typescriptlang.org/"
-                  style={{ backgroundColor: "#286b86", color: "white" }}
-                >
-                  Groovy
-                </Button>
-              </Box>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <Typography variant="body1">
-                Cloud and Web Technologies
-              </Typography>
-            </TableCell>
-            <TableCell>
-              <Box sx={{ display: "flex", gap: "10px" }}>
-                <Button
-                  variant="contained"
-                  size="small"
-                  href="https://www.typescriptlang.org/"
-                  style={{ backgroundColor: "#3C873A", color: "white" }}
-                >
-                  NodeJs
-                </Button>
-                <Button
-                  variant="contained"
-                  size="small"
-                  href="https://expressjs.com/"
-                  style={{ backgroundColor: "#eee", color: "black" }}
-                >
-                  ExpressJs
-                </Button>
-                <Button
-                  variant="contained"
-                  size="small"
-                  href="https://loopback.io/"
-                  style={{
-                    backgroundColor: "#009cff",
-                    color: "white",
-                  }}
-                >
-                  Loopback
-                </Button>
-              </Box>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>
-              <Typography variant="body1">Database & Tools </Typography>
-            </TableCell>
-            <TableCell>
-              <Box sx={{ display: "flex", gap: "10px" }}>
-                <Button
-                  variant="contained"
-                  size="small"
-                  href="https://www.postgresql.org/"
-                  style={{
-                    backgroundColor: "#0064a5",
-                    color: "white",
-                  }}
-                >
-                  PostgresSQL
-                </Button>
-              </Box>
-            </TableCell>
-          </TableRow>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <Typography variant="subtitle1">
+                  Programming Languages
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Box sx={{ display: "flex", gap: "10px" }}>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    href="https://www.typescriptlang.org/"
+                    style={{ backgroundColor: "#007acc", color: "white" }}
+                  >
+                    Typescript
+                  </Button>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    href="https://www.typescriptlang.org/"
+                    style={{ backgroundColor: "#fcdc00", color: "black" }}
+                  >
+                    Javascript
+                  </Button>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    href="https://www.python.org/"
+                    style={{
+                      backgroundColor: "#4584b6",
+                      color: "white",
+                    }}
+                  >
+                    Python
+                  </Button>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    href="https://www.java.com/en/"
+                    style={{
+                      backgroundColor: "#f89820",
+                      color: "white",
+                    }}
+                  >
+                    Java
+                  </Button>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    href="https://learn.microsoft.com/en-us/dotnet/csharp/"
+                    style={{ backgroundColor: "green", color: "white" }}
+                  >
+                    C#
+                  </Button>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    href="https://www.typescriptlang.org/"
+                    style={{ backgroundColor: "#286b86", color: "white" }}
+                  >
+                    Groovy
+                  </Button>
+                </Box>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography variant="body1">
+                  Cloud and Web Technologies
+                </Typography>
+              </TableCell>
+              <TableCell>
+                <Box sx={{ display: "flex", gap: "10px" }}>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    href="https://www.typescriptlang.org/"
+                    style={{ backgroundColor: "#3C873A", color: "white" }}
+                  >
+                    NodeJs
+                  </Button>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    href="https://expressjs.com/"
+                    style={{ backgroundColor: "#eee", color: "black" }}
+                  >
+                    ExpressJs
+                  </Button>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    href="https://loopback.io/"
+                    style={{
+                      backgroundColor: "#009cff",
+                      color: "white",
+                    }}
+                  >
+                    Loopback
+                  </Button>
+                </Box>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <Typography variant="body1">Database & Tools </Typography>
+              </TableCell>
+              <TableCell>
+                <Box sx={{ display: "flex", gap: "10px" }}>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    href="https://www.postgresql.org/"
+                    style={{
+                      backgroundColor: "#0064a5",
+                      color: "white",
+                    }}
+                  >
+                    PostgresSQL
+                  </Button>
+                </Box>
+              </TableCell>
+            </TableRow>
+          </TableBody>
         </Table>
       </TableContainer>
     </Container>
