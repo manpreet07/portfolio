0631a7161e6631682773e282110af062464d55b6
manpreet07 <manpreet1107@gmail.com>
2025-04-05 07:21:21 -0400

    home page

--- src/components/Marsdashboard.tsx
+++ src/components/Marsdashboard.tsx
@@ -1,73 +1,63 @@
-import {
-  Box,
-  Button,
-  Link,
-  TableCell,
-  TableRow,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Link, Typography } from "@mui/material";
 
 export function MarsDashboard() {
   return (
-    <TableRow>
-      <TableCell>MarsDashboard</TableCell>
-      <TableCell>
-        <Box sx={{ display: "flex", gap: "10px" }}>
-          <Button
-            variant="contained"
-            size="small"
-            href="https://www.typescriptlang.org/"
-            style={{ backgroundColor: "#007acc", color: "white" }}
-          >
-            Typescript
-          </Button>
-          <Button
-            variant="contained"
-            size="small"
-            href="https://www.typescriptlang.org/"
-            style={{ backgroundColor: "#fcdc00", color: "black" }}
-          >
-            ReactJS
-          </Button>
-          <Button
-            variant="contained"
-            size="small"
-            href="https://www.typescriptlang.org/"
-            style={{ backgroundColor: "#3C873A", color: "white" }}
-          >
-            NodeJS
-          </Button>
-          <Button
-            variant="contained"
-            size="small"
-            href="https://www.typescriptlang.org/"
-            style={{ backgroundColor: "#007FFF", color: "black" }}
-          >
-            MaterialUI
-          </Button>
-        </Box>
-      </TableCell>
-      <TableCell>
-        <Typography variant="body2" color="text.secondary">
-          Mars Dashboard display images captured by the Mars Rovers
-        </Typography>
-      </TableCell>
-      <TableCell>
+    <Box>
+      <Typography>MarsDashboard</Typography>
+      <Typography variant="body2" color="text.secondary">
+        Mars Dashboard display images captured by the Mars Rovers
+      </Typography>
+      <Box sx={{ display: "flex", gap: "10px" }}>
+        <Button
+          variant="contained"
+          size="small"
+          href="https://www.typescriptlang.org/"
+          style={{ backgroundColor: "#007acc", color: "white" }}
+        >
+          Typescript
+        </Button>
+        <Button
+          variant="contained"
+          size="small"
+          href="https://www.typescriptlang.org/"
+          style={{ backgroundColor: "#fcdc00", color: "black" }}
+        >
+          ReactJS
+        </Button>
+        <Button
+          variant="contained"
+          size="small"
+          href="https://www.typescriptlang.org/"
+          style={{ backgroundColor: "#3C873A", color: "white" }}
+        >
+          NodeJS
+        </Button>
+        <Button
+          variant="contained"
+          size="small"
+          href="https://www.typescriptlang.org/"
+          style={{ backgroundColor: "#007FFF", color: "black" }}
+        >
+          MaterialUI
+        </Button>
+      </Box>
+
+      <Box>
         <Link
           href="https://github.com/manpreet07/mars-dashboard"
           color="inherit"
         >
           gith repo
         </Link>
-      </TableCell>
-      <TableCell>
+      </Box>
+      <Box>
         <Link
           href="https://github.com/manpreet07/mars-dashboard"
           color="inherit"
         >
           live site
         </Link>
-      </TableCell>
-    </TableRow>
+      </Box>
+    </Box>
   );
 }
---
+++ src/declarations.d.ts
@@ -0,0 +1,9 @@
+declare module "portfolio/App" {
+  const App: React.ComponentType;
+  export default App;
+}
+
+declare module "marsDashboard/App" {
+  const App: React.ComponentType;
+  export default App;
+}
